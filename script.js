c3b3f7f840b7477a0fa47dcf2090013121dcc957
Chetu-07 <chetanprakashcs@gmail.com>
2025-04-13 12:04:43 +0000

    return values of scores set to 0 after game over

--- script.js
+++ script.js
@@ -12,42 +12,61 @@ function getComputerChoice(){
     else {
         computerChoice = "scissors";
     }
-    console.log (`computer choose ${computerChoice}`);
     return computerChoice;
 }
 
+let humanScore = 0;
+let computerScore = 0;
+let draw = 0;
 
-
-let gameRound = function gameRound(){
+function gameRound(){
     getHumanChoice = prompt("Rock,Paper or Scissors");
     getHumanChoice = getHumanChoice.toLowerCase();
 
-    let humanScore = 0;
-    let computerScore = 0;
-    let draw = 0;
+    
+    console.log (`computer choose ${computerChoice}`);
     if (getHumanChoice === computerChoice){
         draw += 1;
         console.log ("it\'s a draw")
-        console.log (`You : ${humanScore} , Computer :computerScore, Draws : ${draw}`);
+        console.log (`You : ${humanScore} , Computer :${computerScore}, Draws : ${draw}`);
     }
-    else if (getHumanChoice=== "rock" && computerChoice ==="scissors"
-        || getHumanChoice=== "paper" && computerChoice ==="rock "
-        || getHumanChoice=== "scissors" && computerChoice ==="rock"){
+    else if ((getHumanChoice=== "rock" && computerChoice ==="scissors")
+        || (getHumanChoice=== "paper" && computerChoice ==="rock")
+        || (getHumanChoice=== "scissors" && computerChoice ==="paper")){
             humanScore += 1;
             console.log ("You won");
-            console.log (`You : ${humanScore} , Computer :computerScore, Draws : ${draw}`);
+            console.log (`You : ${humanScore} , Computer :${computerScore}, Draws : ${draw}`);
         }
-    else {
-        humanScore += 1;
-            console.log ("You loose");
+    else {  
             computerScore += 1;
-            console.log (`You : ${humanScore} , Computer :computerScore, Draws : ${draw}`);
+            console.log ("You loose");
+            console.log (`You : ${humanScore} , Computer :${computerScore}, Draws : ${draw}`);
     
     }
+
 }
 
 function playRound(){
-    
     getComputerChoice();
     gameRound();
+}
+function compareScores(){
+    if (humanScore> computerScore){
+        alert("you won");
+    }
+    else if (humanScore< computerScore){
+        alert("you loose");
+    }
+    else {
+        alert("a Draw");
+    }
+}
+function playGame(){
+    for (i=1;i<6;i++){
+        playRound();
+    };
+    compareScores();
+    computerScore=0;
+    humanScore=0;
+    draw =0;
 }
